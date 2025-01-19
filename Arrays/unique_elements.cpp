9998cd2926adb163670a7ce5d4da70b3dea766b7
Tanushree Borase <tanushreebcs@gmail.com>
2025-01-19 17:19:32 +0530

    Create unique_elements.cpp

---
+++ Arrays/unique_elements.cpp
@@ -0,0 +1,18 @@
+int findUnique(int *arr, int size)
+{
+   	int ans = 0;
+    
+    for(int i = 0; i<size; i++) {
+        ans = ans^arr[i];
+    }
+    return ans;
+}
+
+/*
+The XOR operation has some special properties that we will use:
+
+a ^ a = 0 (XOR-ing a number with itself cancels it out)
+a ^ 0 = a (XOR-ing a number with 0 keeps the number unchanged)
+XOR is commutative and associative, meaning the order of applying XOR doesn't matter.
+
+*/
