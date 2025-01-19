a422e7271b4dfafed1e749d950612c102997fd12
Tanushree Borase <tanushreebcs@gmail.com>
2025-01-19 19:23:13 +0530

    Create sort01.cpp

---
+++ Arrays/sort01.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+
+void printArray(int arr[], int n) {
+    for(int i=0; i<n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void sortOne(int arr[], int n) {
+
+    int left = 0, right = n-1;
+
+    while(left < right) {
+
+        while(arr[left] == 0 && left < right )  {
+            left++;
+        }
+
+        while(arr[right]==1 && left < right){
+            right--;
+        }
+
+        //agar yha pohoch gye ho, iska matlab
+        //arr[left]==1 and arr[right]==0
+        if(left<right)
+        {
+            swap(arr[left], arr[right]);
+            left++;
+            right--;
+        }
+    }
+
+}
+
+int main() {
+
+    int arr[8] = {1,1,0,0,0,0,1,0};
+
+    sortOne(arr, 8);
+    printArray(arr, 8);
+
+
+    return 0;
+}
+
+
+/*
+Problem statement
+You have been given an integer array/list(ARR) of size 'N'. It only contains 0s, 1s and 2s. Write a solution to sort this array/list.
+
+Note :
+Try to solve the problem in 'Single Scan'. ' Single Scan' refers to iterating over the array/list just once or to put it in other words, 
+you will be visiting each element in the array/list just once.
+*/
