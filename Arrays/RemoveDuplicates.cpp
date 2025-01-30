cfc5f7c9e76399b05b781bbf2590b5d82a3e142d
Tanushree Borase <tanushreebcs@gmail.com>
2025-01-30 21:47:38 +0530

    Update RemoveDuplicates.cpp

--- Arrays/RemoveDuplicates.cpp
+++ Arrays/RemoveDuplicates.cpp
@@ -19,4 +19,26 @@ Note: Return k after placing the final result in the first k slots of the array.
 
 If arr[j] != arr[i], increase ‘i’ and update arr[i] == arr[j].
  After completion of the loop return i+1, i.e size of the array of unique elements.
+
+ Initialization:
+
+i = 0 initializes the first index of the array. It will keep track of the position where the next unique element should be placed.
+Iterate over the array:
+
+The for loop starts from j = 1 and goes to n-1. This loop checks each element of the array against the element at index i.
+Check for duplicates:
+
+Inside the loop, the condition if (arr[i] != arr[j]) checks if the element at arr[j] is different from the element at arr[i].
+If the elements are different, it means arr[j] is a unique element that hasn’t been seen yet, so it needs to be added 
+to the array of unique elements.
+Update the array:
+
+When a unique element is found, i++ increments the position of the unique element index, and arr[i] = arr[j] places the new unique element 
+at the next available position in the array.
+
+
+Final Result:
+
+After the loop finishes, i + 1 is returned, which represents the new length of the array without duplicates (because i is zero-indexed
+, and we want to count the number of unique elements).
 */
