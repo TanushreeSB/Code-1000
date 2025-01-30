73b2dbebda02c44fefa970b897e7b11ec2306e6e
Tanushree Borase <tanushreebcs@gmail.com>
2025-01-30 21:44:24 +0530

    Create RemoveDuplicates.cpp

---
+++ Arrays/RemoveDuplicates.cpp
@@ -0,0 +1,22 @@
+int removeDuplicates(int arr[], int n)
+{
+  int i = 0;
+  for (int j = 1; j < n; j++) {
+    if (arr[i] != arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
+  }
+  return i + 1;
+}
+
+/*
+Given an integer array sorted in non-decreasing order, remove the duplicates in place such that each unique element appears only once. The relative order of the elements should be kept the same.
+
+If there are k elements after removing the duplicates, then the first k elements of the array should hold the final result. It does not matter what you leave beyond the first k elements.
+
+Note: Return k after placing the final result in the first k slots of the array.
+
+If arr[j] != arr[i], increase ‘i’ and update arr[i] == arr[j].
+ After completion of the loop return i+1, i.e size of the array of unique elements.
+*/
