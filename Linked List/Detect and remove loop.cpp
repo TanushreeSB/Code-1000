f4cbfd06fcc1472546180b23a858033ef604594c
Tanushree Borase <tanushreebcs@gmail.com>
2025-04-13 15:40:23 +0530

    Create Detect and remove loop.cpp

---
+++ Linked List/Detect and remove loop.cpp
@@ -0,0 +1,104 @@
+/*************************************************
+    
+    class Node {
+        public :
+
+        int data;
+        Node *next;
+
+        Node(int data) {
+            this -> data = data;
+            this -> next = NULL;
+        }
+    };
+
+*************************************************/
+
+Node* floydDetectLoop(Node* head) {
+
+    if(head == NULL)
+        return NULL;
+
+    Node* slow = head;
+    Node* fast = head;
+
+    while(slow != NULL && fast !=NULL) {
+        
+        fast = fast -> next;
+        if(fast != NULL) {
+            fast = fast -> next;
+        }
+
+        slow = slow -> next;
+
+        if(slow == fast) {
+            return slow;
+        }
+    }
+
+    return NULL;
+
+}
+
+Node* getStartingNode(Node* head) {
+
+    if(head == NULL) 
+        return NULL;
+
+    Node* intersection = floydDetectLoop(head);
+    
+    if(intersection == NULL)
+        return NULL;
+    
+    Node* slow = head;
+
+    while(slow != intersection) {
+        slow = slow -> next;
+        intersection = intersection -> next;
+    }  
+
+    return slow;
+
+}
+
+Node *removeLoop(Node *head)
+{
+    if( head == NULL)
+        return NULL;
+
+    Node* startOfLoop = getStartingNode(head);
+    
+    if(startOfLoop == NULL)
+        return head;
+    
+    Node* temp = startOfLoop;
+
+    while(temp -> next != startOfLoop) {
+        temp = temp -> next;
+    } 
+
+    temp -> next = NULL;
+    return head;
+}
+
+
+
+/*
+
+Problem statement
+Given a singly linked list, you have to detect the loop and remove the loop from the linked list, if present. You have to make changes in the given linked list itself and return the updated linked list.
+
+Expected Complexity: Try doing it in O(n) time complexity and O(1) space complexity. Here, n is the number of nodes in the linked list.
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints:
+1 <= N <= 100000.
+1 <= ‘VAL’ <= 1000 .  
+
+Time limit: 1 sec
+Sample Input:
+6 2
+1 2 3 4 5 6 
+Sample Output:
+1 2 3 4 5 6
+
+*/
