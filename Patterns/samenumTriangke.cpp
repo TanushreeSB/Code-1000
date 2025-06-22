ad1ec20c71421a32418f7e347bffc69d394e8853
Tanushree Borase <tanushreebcs@gmail.com>
2025-06-22 20:10:48 +0530

    Create samenumTriangke.cpp

---
+++ Patterns/samenumTriangke.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin >> n;
+
+	
+	for(int i=1;i<=n;i++){
+	    for(int j=1;j<=i;j++){
+	        cout << i;
+	        
+	    }
+	    cout << endl;
+	}
+	
+}
+/*
+
+Sample Input
+5
+Your Output
+1
+22
+333
+4444
+55555
+
+*/
