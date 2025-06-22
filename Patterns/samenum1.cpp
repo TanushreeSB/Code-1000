3b1417795ee0c4bb7351b2b68f4476c2b53baa0c
Tanushree Borase <tanushreebcs@gmail.com>
2025-06-22 16:03:46 +0530

    Create samenum1.cpp

---
+++ Patterns/samenum1.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin >> n;
+	
+	for(int i=1;i<=n;i++){
+	    for(int j=1;j<=n;j++){
+	        cout << i;
+	    }
+	    cout << endl;
+	}
+	
+}
+
+/*
+
+5
+11111
+22222
+33333
+44444
+55555
+
+*/
