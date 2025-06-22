52a7f01833ac7b67d2fe56a262772a001e9f66d2
Tanushree Borase <tanushreebcs@gmail.com>
2025-06-22 10:23:03 +0530

    Update sum.cpp

--- basic codes/sum.cpp
+++ basic codes/sum.cpp
@@ -1,12 +1,16 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    int sum = 0;
-    
-    for(int i=0;i<20;i++){
-        sum = sum + i;
+int main() {
+    int n, sum = 0;
+
+    cout << "Enter a positive integer: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {  
+        sum += i;
     }
-    cout << "Sum: " << sum << endl;
+
+    cout << "Sum = " << sum;
     return 0;
 }
