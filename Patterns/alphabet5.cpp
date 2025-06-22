48f64839a858737fe8b1cd6311fb808b6e74df23
Tanushree Borase <tanushreebcs@gmail.com>
2025-06-22 23:30:56 +0530

    Create alphabet5.cpp

---
+++ Patterns/alphabet5.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin >> n;
+    
+	for(int i=1;i<=n;i++){
+	    for(int j=1;j<=i;j++){
+	        char ch = 'A' + i - 1;
+	        cout << ch << " ";
+	        
+	    }
+	    cout << endl;
+	}
+	
+}
+
+/*
+
+Sample Input
+5
+Your Output
+A 
+B B 
+C C C 
+D D D D 
+E E E E E 
+
+*/
