b52489d10610c32bfb4e43f8f1e4c73dfd8ebdfb
Tanushree Borase <tanushreebcs@gmail.com>
2025-06-22 19:49:55 +0530

    Create starTriangle.cpp

---
+++ Patterns/starTriangle.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin >> n;
+
+	
+	for(int i=1;i<=n;i++){
+	    for(int j=1;j<=i;j++){
+	        cout << "*";
+	        
+	    }
+	    cout << endl;
+	}
+	
+}
+
+/*
+
+Sample Input
+5
+Your Output
+*
+**
+***
+****
+*****
+
+*/
