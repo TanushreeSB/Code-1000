e5c65a3b68b3aa09cf627125508a1493a4185c53
Tanushree Borase <tanushreebcs@gmail.com>
2025-06-29 21:56:00 +0530

    Create duplicatesinArray.cpp

---
+++ leetcode/duplicatesinArray.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        // This vector will store the result: the duplicates we find
+        vector<int> ans;
+
+        // Loop over each number in the array
+        for (int i = 0; i < nums.size(); ++i) {
+            // We take the absolute value in case it has been marked negative before
+            int index = abs(nums[i]) - 1;
+
+            // If the number at this index is already negative,
+            // it means we've seen this number before — it's a duplicate!
+            if (nums[index] < 0) {
+                // Since index = num - 1, we add (index + 1) to get the actual number
+                ans.push_back(index + 1);
+            } else {
+                // Mark this number as visited by making the number at its index negative
+                // This only works because the numbers are in the range [1, n]
+                nums[index] = -nums[index];
+            }
+        }
+
+        // Return the result containing all duplicate numbers
+        return ans;
+    }
+};
