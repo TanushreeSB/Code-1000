a64773da3c4aebf5d651109bbc18c2caab1cea7b
Tanushree Borase <tanushreebcs@gmail.com>
2026-02-03 18:15:44 +0530

    Create pointers_array.cpp

---
+++ Pointers/Pointers with Arrays and Functions/pointers_array.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+
+    //int arr[10] = {23, 122, 41, 67};
+
+   /*
+    cout <<" address of first memory block is " << arr << endl; // 0x7ffe3d32a270
+    cout << arr[0] << endl; // 23
+    cout <<" address of first memory block is " << &arr[0] << endl; // 0x7ffe3d32a270
+
+    cout << "4th " << *arr << endl; // 23
+    cout << "5th " << *arr + 1 << endl; // 24
+    cout << "6th " << *(arr + 1) << endl; // 122
+    cout << "7th " << *(arr) + 1 << endl; // 24
+    cout << "8th " << arr[2] << endl; // 41
+    cout << "9th " << *(arr+2) << endl; // 41
