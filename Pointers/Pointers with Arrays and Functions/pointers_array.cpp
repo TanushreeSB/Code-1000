0bad98906b3265132ac294799227fa2e3e069dc8
Tanushree Borase <tanushreebcs@gmail.com>
2026-02-04 15:11:38 +0530

    Update pointers_array.cpp

--- Pointers/Pointers with Arrays and Functions/pointers_array.cpp
+++ Pointers/Pointers with Arrays and Functions/pointers_array.cpp
@@ -20,3 +20,39 @@ int main () {
     int i = 3;
     cout << i[arr] << endl; // 67
 
+    int temp[10] = {1,2};
+    cout << sizeof(temp) << endl; // 40
+    cout << " 1st " <<  sizeof(*temp) << endl; // 4
+    cout << " 2nd " <<  sizeof(&temp) << endl; // 8
+
+    int *ptr = &temp[0];
+    cout << sizeof(ptr) << endl ; // 8
+    cout << sizeof(*ptr) << endl ; // 4
+    cout << sizeof(&ptr) << endl ; // 8
+*/
+
+    int a[20] = {1,2,3,5};
+   //cout << " ->" << &a[0] << endl; //0x7ffcbb9b98d0
+  // cout << &a << endl; // 0x7ffcbb9b98d0
+   //cout << a << endl; // 0x7ffcbb9b98d0
+
+  int *p = &a[0];
+  // cout << p << endl; // 0x7ffd9d530480
+  // cout << *p << endl; // 1
+  // cout << "-> " << &p << endl; //  0x7ffd9d530478
+
+  //   int arr[10];
+
+//ERROR
+   //arr = arr+1;
+
+  //  int *ptr = &arr[0];
+    //cout << ptr << endl; // 0x7fffd522ef10
+  //  ptr = ptr + 1;
+  //  cout << ptr << endl; // 0x7fffd522ef14
+
+    return  0;
+}
+
+
+
