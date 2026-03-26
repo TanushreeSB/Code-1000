ee3d2f914fcb885074cb7b59ad023b8d562e830d
Tanushree Borase <tanushreebcs@gmail.com>
2026-03-26 19:17:22 +0530

    Update Contains Duplicate.cpp

--- Neetcode-150/Contains Duplicate.cpp
+++ Neetcode-150/Contains Duplicate.cpp
@@ -27,3 +27,26 @@ public:
         return false;
     }
 };
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        unordered_set<int>st;
+        for(int i=0;i<n;i++){
+            if(st.count(nums[i])){
+                return true;
+            }
+            st.insert(nums[i]);
+        }
+        return false;
+    }
+};
+
+Time & Space Complexity
+Time complexity: 
+O(n)
+Space complexity: 
+O(n)
