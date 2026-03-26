4e68815d6993275d6f431deaa0430a63d72631ca
Tanushree Borase <tanushreebcs@gmail.com>
2026-03-26 18:22:54 +0530

    Update Contains Duplicate.cpp

--- Neetcode-150/Contains Duplicate.cpp
+++ Neetcode-150/Contains Duplicate.cpp
@@ -12,3 +12,18 @@ public:
         return false;
     }
 };
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == nums[i + 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
