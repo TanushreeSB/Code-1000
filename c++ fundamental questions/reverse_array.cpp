762c2fb172ddf2bb36dcb45fa586f373eee3ffd1
Tanushree Borase <tanushreebcs@gmail.com>
2026-04-05 16:09:12 +0530

    Create reverse_array.cpp

---
+++ c++ fundamental questions/reverse_array.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+	int arr[5];
+	for(int i=0;i<5;i++){
+	    cin >> arr[i];
+	}
+	
+	cout << "Reversed array: " << endl;
+	for(int i=4;i>=0;i--){
+	    cout << arr[i] << " ";
+	}
+
+}
