21c406ff67f2ba114709663ef1607a39d43808ac
Tanushree Borase <tanushreebcs@gmail.com>
2026-04-05 16:25:29 +0530

    Update reverse_array.cpp

--- c++ fundamental questions/reverse_array.cpp
+++ c++ fundamental questions/reverse_array.cpp
@@ -14,3 +14,34 @@ int main() {
 	}
 
 }
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int left = 0, right = n - 1;
+    while(left < right) {
+        int temp = arr[left]; 
+        arr[left] = arr[right];
+        arr[right] = temp;
+// or can use swap(arr[left],arr[right]
+        left++;
+        right--;
+    }
+
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
