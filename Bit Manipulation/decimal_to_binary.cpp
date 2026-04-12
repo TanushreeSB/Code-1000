c7049abb6aa3ec841f5777d04953fb75134513da
Tanushree Borase <tanushreebcs@gmail.com>
2026-04-12 17:32:12 +0530

    Update decimal_to_binary.cpp

--- Bit Manipulation/decimal_to_binary.cpp
+++ Bit Manipulation/decimal_to_binary.cpp
@@ -18,3 +18,29 @@ int main(){
     cout << "answer is: " << ans;
     return 0;
 }
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    string binary = "";
+
+    if (n == 0) {
+        cout << 0;
+        return 0;
+    }
+
+    while (n > 0) {
+        binary += (n % 2) + '0';
+        n = n / 2;
+    }
+
+    reverse(binary.begin(), binary.end());
+
+    cout << binary;
+}
